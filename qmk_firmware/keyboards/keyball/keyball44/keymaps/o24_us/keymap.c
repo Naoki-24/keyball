6b7ff1d0338c270684d4ccf38aa4510086518a31
Naoki Miyamoto <naoki.miyamoto@almondotech.com>
2024-12-25 16:08:58 +0900

    fix: my bracket

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/o24_us/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/o24_us/keymap.c
@@ -131,8 +131,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case MY_BRCT:
       if (record->event.pressed) {
         SEND_STRING("[]");
-        return false;
       }
+      return false;
   }
   return true;
 }
