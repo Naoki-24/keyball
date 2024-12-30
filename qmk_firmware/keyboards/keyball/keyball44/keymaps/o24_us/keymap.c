e672e6cee2243111d868ba97257af29d8ebe02c2
naoki <naoki524miyamoto@gmail.com>
2024-12-30 23:53:43 +0900

    swap thumb

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/o24_us/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/o24_us/keymap.c
@@ -55,7 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T   ,                                       KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , JP_EQL   ,
     KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G   ,                                       KC_H     , KC_J     , KC_K     , KC_L     , JP_EQL   , JP_QUOT  ,
     CTL_PRNS , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B   ,                                       KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLASH , JP_SCLN ,
-               KC_LGUI  , ALT_BRCS,                   SFT_GRV , KC_BTN1, LY1_SPC,                 KC_BSPC  , LY2_ENT    , KC_0     , KC_PSCR  , TG(3)
+               KC_LGUI  , ALT_BRCS,                   LY1_SPC , KC_BTN1, SFT_GRV,                 KC_BSPC  , LY2_ENT    , KC_0     , KC_PSCR  , TG(3)
   ),
 };
 
