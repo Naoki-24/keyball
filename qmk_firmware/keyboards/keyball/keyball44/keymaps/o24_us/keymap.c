b8b4e609a2378d4012995b822e3756339fc20a57
naoki <naoki524miyamoto@gmail.com>
2025-01-02 00:04:47 +0900

    update: TL on layer3

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/o24_us/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/o24_us/keymap.c
@@ -53,14 +53,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [3] = LAYOUT_universal(
     KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T   ,                                       KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , JP_EQL   ,
-    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G   ,                                       KC_H     , KC_J     , KC_K     , KC_L     , JP_EQL   , JP_QUOT  ,
-    CTL_PRNS , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B   ,                                       KC_N     , KC_M     , KC_COMM  , KC_DOT   , KC_SLASH , JP_SCLN ,
+    KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G   ,                                       KC_H     , KC_J     , KC_K     , KC_L     , JP_SCLN  , JP_QUOT  ,
+    CTL_PRNS , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B   ,                                       KC_N     , KC_M     , KC_COMM  , KC_DOT   , JP_MINS  , KC_SLASH ,
                KC_LGUI  , ALT_BRCS,                   LY1_SPC , KC_BTN1, SFT_GRV,                 KC_BSPC  , LY2_ENT    , KC_0     , KC_PSCR  , TG(3)
   ),
 };
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+    case LY2_ENT:
+      if (IS_LAYER_ON(3)) {
+        if (record->event.pressed) {
+          layer_on(2);
+          return false;
+        } else {
+          layer_off(2);
+          return false;
+        }
+      }
+    case LY1_SPC:
+      if (IS_LAYER_ON(3)) {
+        if (record->event.pressed) {
+          layer_on(1);
+          return false;
+        } else {
+          layer_off(1);
+          return false;
+        }
+      }
     case JP_MINS:
       if (record->event.pressed) {
         if (get_mods() & MOD_MASK_SHIFT) {
