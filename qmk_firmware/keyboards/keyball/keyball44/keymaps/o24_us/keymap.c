b08fd3c214c297489686466343a9a88b26c51502
naoki <naoki524miyamoto@gmail.com>
2025-11-02 04:09:13 +0900

    fix shift

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/o24_us/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/o24_us/keymap.c
@@ -24,7 +24,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define CTL_PRNS MT(MOD_LCTL, KC_8)
 #define SFT_SPC MT(MOD_LSFT, KC_SPC)
 #define OS_BRCS MT(MOD_LGUI, KC_LBRC)
-#define SHFT_SPC MT(MOD_LSFT, KC_SPC)
 #define LY2_ENT LT(2, KC_ENT)
 #define LY3_INT4 LT(3, KC_INT4)
 
@@ -129,7 +128,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC   , KC_Q     , KC_W     , KC_E     , KC_R     , KC_T   ,                                       KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_BSPC  ,
     KC_TAB   , KC_A     , KC_S     , KC_D     , KC_F     , KC_G   ,                                       KC_H     , KC_J     , KC_K     , KC_L     , JP_SCLN  , JP_QUOT  ,
     CTL_PRNS , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B   ,                                       KC_N     , KC_M     , KC_COMM  , KC_DOT   , JP_MINS  , KC_SLASH ,
-               OS_BRCS  , TD(TD_ALT_TG1) ,     LY2_ENT, KC_BTN1, TD(TD_MUHENKAN_HENKAN),              LY3_INT4    , SHFT_SPC , KC_0     , KC_PSCR  , JP_EQL
+               OS_BRCS  , TD(TD_ALT_TG1) ,     LY2_ENT, KC_BTN1, TD(TD_MUHENKAN_HENKAN),              LY3_INT4    , SFT_SPC , KC_0     , KC_PSCR  , JP_EQL
   ),
 
   [1] = LAYOUT_universal(
